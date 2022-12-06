307a5920f8c3be7ab4fac4763000552aa6d5fe4b
Thomas Fike <fike@me.com>
2022-12-05 23:32:34 -0700

    Day 6 Part 1

---
+++ Day6/day6.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file day6.cpp
+ */
+
+#include "common.hpp"
+
+template <size_t SIZE>
+class Viewer {
+ public:
+  Viewer(void) { data.fill('\0'); }
+  void addItem(const char item) {
+    const auto findIter = std::find(data.begin(), data.end(), '\0');
+    if (findIter != data.end()) {
+      *findIter = item;
+    } else {
+      std::rotate(data.begin(), data.begin() + 1, data.end());
+      data[data.size() - 1] = item;
+    }
+  }
+
+  [[nodiscard]] bool unique(void) const {
+    auto copy = data;
+    std::sort(copy.begin(), copy.end());
+    return std::unique(copy.begin(), copy.end()) == copy.end() && isFilled();
+  }
+
+  [[nodiscard]] const std::array<char, SIZE>& getData(void) const {
+    return data;
+  }
+
+ private:
+  std::array<char, SIZE> data;
+  bool isFilled(void) const {
+    const auto findIter = std::find(data.begin(), data.end(), '\0');
+    return findIter == data.end();
+  }
+};
+
+int main() {
+  std::ifstream inFile("part1.txt");
+  Viewer<4> viewer;
+  int counter = 0;
+  while (inFile.good()) {
+    char temp;
+    inFile >> temp;
+    viewer.addItem(temp);
+    counter++;
+    if (viewer.unique()) {
+      break;
+    }
+  }
+  fmt::print("Done {}\n", counter);
+}
