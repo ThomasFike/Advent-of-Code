4896ec6c38e09cb6d5db1cb43b28e61eb142caf6
Thomas Fike <fike@me.com>
2024-12-30 12:34:13 -0700

    Day 3 Part 2

--- Day3/day3.cpp
+++ Day3/day3.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <algorithm>
+#include <expected>
 #include <fstream>
 #include <iostream>
 #include <numeric>
@@ -12,21 +13,47 @@
 
 static const auto FileName = "part1.txt";
 
-static const std::regex mulMatcher(R"~(mul\((\d{1,3}),(\d{1,3})\))~");
+enum Allowed {
+  Do,
+  Dont,
+};
+
+using Ops = std::pair<int, int>;
+using Founds = std::expected<int, Allowed>;
+
+static const std::regex mulMatcher(
+    R"~(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))~");
 
 int main() {
   std::ifstream inputFile(FileName);
   std::string line;
   std::getline(inputFile, line, '\0');
-  const auto answer = std::accumulate(
-      std::sregex_iterator(line.begin(), line.end(), mulMatcher),
-      std::sregex_iterator(), 0, [](const int init, const std::smatch& val) {
-        if (val.size() != 3) {
-          throw std::logic_error("You done goofed");
-        }
-        const auto a = std::stoi(val[1]);
-        const auto b = std::stoi(val[2]);
-        return init + (a * b);
-      });
+  std::vector<Founds> matches;
+  for (auto iter = std::sregex_iterator(line.begin(), line.end(), mulMatcher);
+       iter != std::sregex_iterator(); iter++) {
+    const std::smatch& match = *iter;
+    if (match.size() != 3) {
+      throw std::logic_error("Invalid size");
+    }
+
+    if (match[0] == "don't()") {
+      matches.emplace_back(std::unexpected(Allowed::Dont));
+    } else if (match[0] == "do()") {
+      matches.emplace_back(std::unexpected(Allowed::Do));
+    } else {
+      const auto a = std::stoi(match[1]);
+      const auto b = std::stoi(match[2]);
+      matches.emplace_back(a * b);
+    }
+  }
+  auto lastAllowed = Allowed::Do;
+  int answer = 0;
+  for (const auto& item : matches) {
+    if (item && lastAllowed == Allowed::Do) {
+      answer += item.value();
+    } else {
+      lastAllowed = item.error();
+    }
+  }
   std::cout << "Answer: " << answer << std::endl;
 }
