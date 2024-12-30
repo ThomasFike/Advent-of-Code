1a99e832ec3a43e24c8d5fd4e0c47186eb877920
Thomas Fike <fike@me.com>
2024-12-30 10:10:37 -0700

    Day 1 part 2

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -25,11 +25,41 @@ int main() {
 
   std::ranges::sort(list1);
   std::ranges::sort(list2);
+  int answer = 0;
 
-  std::ranges::zip_view view(list1, list2);
-  auto values = view | std::ranges::views::transform([](auto aTuple) {
-                  return std::abs(std::get<1>(aTuple) - std::get<0>(aTuple));
-                });
-  const auto value = std::ranges::fold_left(values, 0, std::plus<int>());
-  std::cout << "Answer: " << value << std::endl;
+  for (auto lIter = list1.cbegin(), rIter = list2.cbegin();
+       lIter != list1.cend() && rIter != list2.cend();) {
+    const auto value = *lIter;
+    // std::cout << "Doing check for: " << value << std::endl;
+    const auto isNotValue = [&](const auto aValue) { return value != aValue; };
+    // See how many copies exist in left list
+    const auto endOfLRange = std::find_if(lIter, list1.cend(), isNotValue);
+
+    // Subtract 1 because it's one past the end
+    const auto leftCount = std::distance(lIter, endOfLRange);
+
+    // Move lIter
+    lIter = endOfLRange;
+
+    // Move rIter
+    rIter = std::find_if(rIter, list2.cend(), [value](const auto aValue) {
+      return aValue >= value;
+    });
+
+    if (rIter == list2.cend() || *rIter != value) {
+      continue;
+    }
+
+    const auto endOfRRange = std::find_if(rIter, list2.cend(), isNotValue);
+    const auto rightCount = std::distance(rIter, endOfRRange);
+
+    // Move rIter
+    rIter = endOfRRange;
+
+    // Incrent answer
+    // std::cout << "Left: " << leftCount << ", Right: " << rightCount
+    //           << std::endl;
+    answer += leftCount * rightCount * value;
+  }
+  std::cout << "Answer: " << answer << std::endl;
 }
