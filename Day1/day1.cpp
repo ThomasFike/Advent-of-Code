3b1125591d9ae04fb1afdd6bc723f2c44176020f
Thomas Fike <fike@me.com>
2024-12-30 09:43:38 -0700

    Fixups to part 1

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -9,28 +9,23 @@
 #include <utility>
 #include <vector>
 
-static const auto FileName = "small.txt";
+static const auto FileName = "part1.txt";
 
 int main() {
   std::ifstream inputFile(FileName);
   std::vector<int> list1;
   std::vector<int> list2;
-  while (inputFile) {
-    int temp1;
-    int temp2;
+  while (inputFile && !inputFile.eof()) {
+    int temp1 = 0;
+    int temp2 = 0;
     inputFile >> temp1 >> temp2;
     list1.emplace_back(temp1);
     list2.emplace_back(temp2);
   }
-  std::cout << "read done" << std::endl;
-  for (size_t i; i < list1.size(); i++) {
-    std::cout << '(' << list1.at(i) << ',' << list2.at(i) << ")\r\n";
-  }
-
-  std::cout << "after sort" << std::endl;
 
   std::ranges::sort(list1);
   std::ranges::sort(list2);
+
   std::ranges::zip_view view(list1, list2);
   auto values = view | std::ranges::views::transform([](auto aTuple) {
                   return std::abs(std::get<1>(aTuple) - std::get<0>(aTuple));
