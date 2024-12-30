af21a404037a5b3650536c21f40f050333b22a41
Thomas Fike <fike@me.com>
2024-12-30 11:35:52 -0700

    Day 2 Part 2

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <algorithm>
+#include <expected>
 #include <fstream>
 #include <iostream>
 #include <ranges>
@@ -11,22 +12,35 @@
 
 static const auto FileName = "part1.txt";
 
-static bool isSafe(const std::vector<int> aRow) {
-  if (aRow.size() < 2) {
-    return false;
-  }
+static std::expected<void, size_t> isSafeOnce(const auto& aRow) {
   const bool isAscending = aRow[1] > aRow[0];
+
   for (size_t i = 0; i < aRow.size() - 1; i++) {
-    const bool pairIsAscending = aRow[i + 1] > aRow[i];
-    if (isAscending != pairIsAscending) {
-      return false;
+    if (aRow.size() < 2) {
+      throw std::logic_error("Invalid");
     }
+    const bool pairIsAscending = aRow[i + 1] > aRow[i];
     const auto delta = std::abs(aRow[i + 1] - aRow[i]);
-    if (delta <= 0 || delta > 3) {
-      return false;
+    if (isAscending != pairIsAscending || (delta <= 0 || delta > 3)) {
+      return std::unexpected(i);
     }
   }
-  return true;
+  return {};
+}
+
+static bool isSafeWithRemoveBrutForce(const auto& aRow) {
+  const auto isSafeFirst = isSafeOnce(aRow);
+  if (isSafeFirst) {
+    return true;
+  }
+  for (size_t i = 0; i < aRow.size(); i++) {
+    std::vector temp = aRow;
+    temp.erase(temp.begin() + i);
+    if (isSafeOnce(temp)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 int main() {
@@ -53,7 +67,7 @@ int main() {
   //   std::cout << std::endl;
   // }
   const auto safeCount =
-      std::count_if(rows.cbegin(), rows.cend(),
-                    [](const auto& aRow) { return isSafe(aRow); });
+      std::count_if(rows.begin(), rows.end(),
+                    [](auto& aRow) { return isSafeWithRemoveBrutForce(aRow); });
   std::cout << "Safe Count: " << safeCount << std::endl;
 }
